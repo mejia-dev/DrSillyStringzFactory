d4d2a3326963967e6678fc3541d435fe94530d07
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-09 10:33:29 -0800

    Add Engineer and License entities

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -7,7 +7,7 @@ namespace Factory.Models
     public int EngineerId { get; set; }
     public string EngineerFirstName { get; set; }
     public string EngineerLastName { get; set; }
-    public List<string> EngineerLicenses { get; set; }
+    // public List<string> EngineerLicenses { get; set; }
     public string EngineerFullName => $"{EngineerFirstName} {EngineerLastName}";
   }
 }
---
+++ Factory/Models/License.cs
@@ -0,0 +1,8 @@
+namespace Factory.Models
+{
+  public class License 
+  {
+    public int LicenseId { get; set; }
+    public string LicenseName { get; set; }
+  }
+}
