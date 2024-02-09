9d50f878a57ffeaca9c1be91a04a011dcfc6d86b
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-09 15:08:57 -0800

    WIP commit -- nearly full CRUD but need multiple selections for Machine

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -36,5 +36,46 @@ namespace Factory.Controllers
       List<Engineer> model = _db.Engineers.ToList();
       return View("Index", model);
     }
+
+    public ActionResult Details(int id)
+    {
+      Engineer thisEngineer = _db.Engineers
+          .Include(eng => eng.AssignedMachines)
+          .FirstOrDefault(eng => eng.EngineerId == id);
+      ViewBag.PageTitle = $"Engineer Details - {thisEngineer.EngineerFullName} ";
+      return View(thisEngineer);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engs => engs.EngineerId == id);
+      ViewBag.PageTitle = $"Edit Engineer - {thisEngineer.EngineerFullName}";
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineName");
+      return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Engineer eng)
+    {
+      _db.Engineers.Update(eng);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engs => engs.EngineerId == id);
+      ViewBag.PageTitle = $"Delete Engineer - {thisEngineer.EngineerFullName}";
+      return View(thisEngineer);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engs => engs.EngineerId == id);
+      _db.Engineers.Remove(thisEngineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
