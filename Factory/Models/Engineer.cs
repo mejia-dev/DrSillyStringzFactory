201815acf2b4c4edd4f3efac33992d83ca328d1c
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-11 10:40:01 -0800

    Add data validation to model

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "The engineer must have a first name.")]
     public string EngineerFirstName { get; set; }
+    [Required(ErrorMessage = "The engineer must have a last name.")]
     public string EngineerLastName { get; set; }
     public List<EngineerMachine> AssignedMachines { get; }
     public string EngineerFullName => $"{EngineerFirstName} {EngineerLastName}";
